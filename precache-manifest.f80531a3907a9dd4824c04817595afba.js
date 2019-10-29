6ce4e792fefff94f70fbccf4969828660ec76e79
davidpfahler <david@excellenteasy.com>
2019-10-29 20:04:38 +0000

    Deploying to gh-pages from master d0d6351db85d1f95c7c6db2c04e39e063ae51cfc

---
+++ precache-manifest.f80531a3907a9dd4824c04817595afba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "406a8a09a8db7f3cc60f39af70d5ac08",
+    "url": "/react-ml-app/index.html"
+  },
+  {
+    "revision": "258888755ac5d46b31c6",
+    "url": "/react-ml-app/static/js/2.a72cfb6d.chunk.js"
+  },
+  {
+    "revision": "e0b6fdcbb1789fb27c6c",
+    "url": "/react-ml-app/static/js/main.17898034.chunk.js"
+  },
+  {
+    "revision": "831dc884f2036a927f5c",
+    "url": "/react-ml-app/static/js/runtime-main.038294a1.js"
+  },
+  {
+    "revision": "e15a286155536bf7a2872a2f0846b550",
+    "url": "/react-ml-app/static/media/dogs-resnet18.e15a2861.onnx"
+  }
+]);
